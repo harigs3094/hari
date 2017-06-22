7491d1f0763f33335268e41e1c8ddf60a80cfa9a
harigs3094 <harivignesh3094@gmail.com>
2017-06-22 10:18:57 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,12 @@
+#find largest number
+fs=int(input("Enter the first number:"))
+ss=int(input("Enter the 2nd number:"))
+td=int(input("Enter the 3rd number:"))
+
+if fs>ss and fs>td:
+   print "the first number is largest number"
+elif ss>td:
+   print "the second number is largest number"
+else:
+   print "the third number is largest number"
+    
